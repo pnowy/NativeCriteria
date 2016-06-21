cabee8055316018e6ab2c87a1ddbd7db394836a9
pnowy <przemek.nowak.pl@gmail.com>
2016-06-21 08:16:20 +0200

    Documentation script update

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-now=$(date +"%Y_%m_%d")
-PROJECT_PATH="D:/projects/NativeCriteria/"
+now=$(date +"%Y-%m-%d")
+SRC_PROJECT_PATH="D:/projects/NativeCriteria/"
 DOCS_PROJECT_PATH="D:/projects/NativeCriteriaPages/"
 
+mvn clean
 couscous generate
 cd $DOCS_PROJECT_PATH
 git rm -r *
-cd $PROJECT_PATH
+cd $SRC_PROJECT_PATH
 cp -av ./.couscous/generated/* $DOCS_PROJECT_PATH
 cd $DOCS_PROJECT_PATH
 git save "Documentation update $now"
